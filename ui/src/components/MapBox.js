93c003c502cc1e556200780d55e8df57822690b0
belcortes <belcortes@gmail.com>
2018-04-12 11:35:14 -0700

    mapping broke, but added results factory

--- ui/src/components/MapBox.js
+++ ui/src/components/MapBox.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet'
+
+import MarkerComponent from './MarkerComponent'
 
 const stamenTonerTiles = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const stamenTonerAttr = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
@@ -8,11 +11,30 @@ const zoomLevel = 10;
 
 class MapBox extends Component {
 	state = {
-    lat: 40.701835,
-    lng: -73.941262
-  }
+		results: [
+			{
+				title: "address1",
+				lat: 40.701835,
+	    	lng: -73.941262
+			},
+			{
+				title: "address2",
+				lat: 41.701835,
+	    	lng: -72.941262
+			} 
+  	]
+	}
   render() {
-  	const position = [this.state.lat, this.state.lng]
+  	const image = L.Icon({
+       iconUrl: "http://gkv.com/wp-content/uploads/leaflet-maps-marker-icons/map_marker-red-small.png",
+       // shadowUrl: require('../public/marker-shadow.png'),
+       iconSize:     [38, 95], // size of the icon
+       shadowSize:   [50, 64], // size of the shadow
+       iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+       shadowAnchor: [4, 62],  // the same for the shadow
+       popupAnchor:  [-3, -76]// point from which the popup should open relative to the iconAnchor
+   })
+  	
     return (
       <div>
         <Map
@@ -21,13 +43,12 @@ class MapBox extends Component {
           <TileLayer
               attribution={stamenTonerAttr}
               url={stamenTonerTiles} />
-          <Marker position={position}>
-	          <Popup>
-	            <span>
-	              A pretty CSS3 popup. <br /> Easily customizable.
-	            </span>
-	          </Popup>
-	        </Marker>
+          {this.state.results.map((result, index) => {
+          	<Marker key={result.index} position={[result.lat, result.lng]} icon={image} />
+          	// <MarkerComponent title={result.title} lat={result.lat} lng={result.lng} index={index} />
+          	// console.log('hello')
+          })}
+          
         </Map>
       </div>
     );
---
+++ ui/src/components/MarkerComponent.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import L from 'leaflet'
+import { Marker, Popup } from 'react-leaflet';
+    
+const MarkerComponent = (props) => {
+
+	const image = L.Icon({
+       iconUrl: "http://gkv.com/wp-content/uploads/leaflet-maps-marker-icons/map_marker-red-small.png",
+       // shadowUrl: require('../public/marker-shadow.png'),
+       iconSize:     [38, 95], // size of the icon
+       shadowSize:   [50, 64], // size of the shadow
+       iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+       shadowAnchor: [4, 62],  // the same for the shadow
+       popupAnchor:  [-3, -76]// point from which the popup should open relative to the iconAnchor
+   })
+
+	const position = [props.lat, props.lng]
+
+  return (
+  	<div>
+	    <Marker key={props.index} position={position} icon={image}>
+	    	<Popup>
+	        <span>
+	          {props.title}
+	        </span>
+	      </Popup>
+	    </Marker>
+    </div>
+  )
+}
+
+export default MarkerComponent
